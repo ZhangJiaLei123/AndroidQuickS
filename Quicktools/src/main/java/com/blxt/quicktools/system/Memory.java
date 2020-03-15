82af08bb1efa1710139c97eddb08133815e7b51a
ZhangJiaLei <zhang.jialei@outlook.com>
2020-03-16 00:12:01 +0800

    更新配置

--- Quicktools/src/main/java/com/blxt/quicktools/system/Memory.java
+++ Quicktools/src/main/java/com/blxt/quicktools/system/Memory.java
@@ -1,64 +1,62 @@
 package com.blxt.quicktools.system;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Environment;
 import android.os.StatFs;
 import android.text.format.Formatter;
-
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class Memory {
     private static final String TAG = Memory.class.getName();
+
+
     private static final String MEM_INFO_PATH = "/proc/meminfo";
+
+
     public static final String MEMTOTAL = "MemTotal";
+
     public static final String MEMFREE = "MemFree";
 
 
-    /**
-     * 得到中内存大小
-     *
-     * @param context
-     * @return
-     */
-    public static String getTotalMemory(Context context) {
-        return getMemInfoIype(context, MEMTOTAL);
+    public static long getMemoryAll(Context context) {
+        String path = Environment.getDataDirectory().getPath();
+        StatFs statFs = new StatFs(path);
+        long blockSize = statFs.getBlockSize();
+        long totalBlocks = statFs.getBlockCount();
+        long availableBlocks = statFs.getAvailableBlocks();
+        long useBlocks = totalBlocks - availableBlocks;
+
+        long rom_length = totalBlocks * blockSize;
+
+        return rom_length;
     }
 
-    /**
-     * 得到可用内存大小
-     *
-     * @param context
-     * @return
-     */
-    public static String getMemoryFree(Context context) {
-        return getMemInfoIype(context, MEMFREE);
-    }
+    public static String getTotalMemory(Context context) { return getMemInfoIype(context, "MemTotal"); }
 
-    /**
-     * 得到type info
-     *
-     * @param context
-     * @param type
-     * @return
-     */
+    public static String getMemoryFree(Context context) { return getMemInfoIype(context, "MemFree"); }
+
+
     public static String getMemInfoIype(Context context, String type) {
         try {
-            FileReader fileReader = new FileReader(MEM_INFO_PATH);
-            BufferedReader bufferedReader = new BufferedReader(fileReader, 4 * 1024);
-            String str = null;
-            while ((str = bufferedReader.readLine()) != null) {
-                if (str.contains(type)) {
-                    break;
-                }
-            }
+            FileReader fileReader = new FileReader("/proc/meminfo");
+            BufferedReader bufferedReader = new BufferedReader(fileReader, 4096);
+            String str = null; do {  }
+            while ((str = bufferedReader.readLine()) != null &&
+                            !str.contains(type));
+
+
+
             bufferedReader.close();
-            /* \\s表示   空格,回车,换行等空白符,
-            +号表示一个或多个的意思     */
+
+
             String[] array = str.split("\\s+");
-            // 获得系统总内存，单位是KB，乘以1024转换为Byte
+
             int length = Integer.valueOf(array[1]).intValue() * 1024;
             return Formatter.formatFileSize(context, length);
         } catch (FileNotFoundException e) {
@@ -69,43 +67,69 @@ public class Memory {
         return null;
     }
 
-    /**
-     * 得到内置存储空间的总容量
-     * @param context
-     * @return
-     */
-    public static String getMemoryAll(Context context){
-        String path = Environment.getDataDirectory().getPath();
-        StatFs statFs = new StatFs(path);
-        long blockSize = statFs.getBlockSize();
-        long totalBlocks = statFs.getBlockCount();
-        long availableBlocks = statFs.getAvailableBlocks();
-        long useBlocks  = totalBlocks - availableBlocks;
+    @SuppressLint({"NewApi"})
+    public static long getSdSize() {
+        File sdcardDir = Environment.getExternalStorageDirectory();
 
-        long rom_length = totalBlocks*blockSize;
+        StatFs statFs = new StatFs(sdcardDir.getPath());
+        long blockSize = statFs.getBlockSizeLong();
 
-        return Formatter.formatFileSize(context,rom_length);
+        long totalSize = statFs.getBlockCountLong();
+
+        return blockSize * totalSize;
     }
 
-    /**
-     * 字节Long 转换文件大小
-     * @param fileS
-     * @return B/KB/MB/GB
-     */
-    public static String getMemSizeStr(long fileS) {
-        java.text.DecimalFormat df = new java.text.DecimalFormat("#.00");
+    public static long getSdAvailableSize() {
+        String sdcard = Environment.getExternalStorageState();
+        String state = "mounted";
+        File file = Environment.getExternalStorageDirectory();
+        StatFs statFs = new StatFs(file.getPath());
+        if (sdcard.equals(state)) {
+
+
+            long blockSize = statFs.getBlockSize();
+
+            long blockavailable = statFs.getAvailableBlocks();
+
+            long blockavailableTotal = blockSize * blockavailable / 1000L / 1000L;
+            return blockSize * blockavailable;
+        }
+
+        return -1L;
+    }
+
+    @SuppressLint({"NewApi"})
+    public static long getRomSize() {
+        File path = Environment.getDataDirectory();
+        StatFs statFs = new StatFs(path.getPath());
+        long blockSize = statFs.getBlockSizeLong();
+        long tatalBlocks = statFs.getBlockCountLong();
+        return blockSize * tatalBlocks;
+    }
+
+    @SuppressLint({"NewApi"})
+    public static long getRomAvailableSize() {
+        File path = Environment.getDataDirectory();
+        StatFs statFs = new StatFs(path.getPath());
+        long blockSize = statFs.getBlockSizeLong();
+        long availableBlocks = statFs.getAvailableBlocksLong();
+        return blockSize * availableBlocks;
+    }
+
+    public static String getSizeStr(Context context, long size) { return Formatter.formatFileSize(context, size); }
+
+    public static String getSizeStr2(long fileS) {
+        DecimalFormat df = new DecimalFormat("#.00");
         String fileSizeString = "";
-        if (fileS < 1024) {
-            fileSizeString = df.format((double) fileS) + "B";
-        } else if (fileS < 1048576) {
-            fileSizeString = df.format((double) fileS / 1024) + "KB";
-        } else if (fileS < 1073741824) {
-            fileSizeString = df.format((double) fileS / 1048576) + "MB";
+        if (fileS < 1024L) {
+            fileSizeString = df.format(fileS) + "B";
+        } else if (fileS < 1048576L) {
+            fileSizeString = df.format(fileS / 1024.0D) + "KB";
+        } else if (fileS < 1073741824L) {
+            fileSizeString = df.format(fileS / 1048576.0D) + "MB";
         } else {
-            fileSizeString = df.format((double) fileS / 1073741824) + "G";
+            fileSizeString = df.format(fileS / 1.073741824E9D) + "G";
         }
         return fileSizeString;
     }
-
-
 }
