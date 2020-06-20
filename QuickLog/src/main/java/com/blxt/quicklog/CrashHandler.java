acaf2d165e2896401a408ec289d96600cd792f8d
ZhangJiaLei <zhang.jialei@outlook.com>
2020-06-20 19:29:05 +0800

    更新

--- QuickLog/src/main/java/com/blxt/quicklog/CrashHandler.java
+++ QuickLog/src/main/java/com/blxt/quicklog/CrashHandler.java
@@ -1,67 +1,217 @@
- package com.blxt.quicklog;
+package com.blxt.quicklog;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
 import androidx.annotation.NonNull;
+
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class CrashHandler
-  implements Thread.UncaughtExceptionHandler
-{
-  static final String TAG = "CrashHandler";
-  static File fileCrash;
-  CrashCallBack crashCallBack = null;
+        implements Thread.UncaughtExceptionHandler {
+    static final String TAG = "CrashHandler";
+    private static CrashHandler INSTANCE;// CrashHandler实例
 
-  public CrashHandler(Context context, String fileName) { this(context.getCacheDir().getPath(), fileName); }
+    CrashCallBack crashCallBack = null;
+    Context context;
 
-  public CrashHandler(String logFilePath, String fileName) {
-    File filePath = new File(logFilePath);
-    if (!filePath.exists()) {
-      filePath.mkdirs();
+    static File fileCrash;
+
+    /**
+     * 获取CrashHandler实例 ,单例模式
+     */
+    public static CrashHandler getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = new CrashHandler(context);
+        }
+        return INSTANCE;
     }
 
-    fileCrash = new File(logFilePath + fileName);
-  }
 
-  public void uncaughtException(@NonNull Thread thread, @NonNull Throwable throwable) {
-    boolean isSave = true;
-    if (this.crashCallBack != null) {
-      isSave = this.crashCallBack.onCrash(throwable);
+    /**
+     * 保证只有一个CrashHandler实例
+     */
+    private CrashHandler(Context context) {
+        this(context, "/CrashLog.log");
+        Thread.setDefaultUncaughtExceptionHandler(this);// 设置该CrashHandler为程序的默认处理器
     }
 
-    if (!isSave) {
-      return;
+    public CrashHandler(Context context, String fileName) {
+        this(context.getCacheDir().getPath(), fileName);
+        this.context = context;
     }
 
-    String time = QLog.getTimeStr("yyyyMMddHHmmss");
-    QLog.e(new Object[] { "CrashHandler", "产生致命异常,见日志:", time, throwable.getMessage() });
-    throwable.printStackTrace();
-    QLog.addFile(fileCrash,
-        String.format("\r\n============= %s ============\r\n %s =============================\r\n", new Object[] {
-
-
-            time, toString(throwable)
-          }));
-  }
-
-  private String toString(Throwable ex) {
-    Writer writer = new StringWriter();
-    PrintWriter printWriter = new PrintWriter(writer);
-    ex.printStackTrace(printWriter);
-    Throwable exCause = ex.getCause();
-    while (exCause != null) {
-      exCause.printStackTrace(printWriter);
-      exCause = exCause.getCause();
+    public CrashHandler(String logFilePath, String fileName) {
+        File filePath = new File(logFilePath);
+        if (!filePath.exists()) {
+            filePath.mkdirs();
+        }
+        fileCrash = new File(logFilePath + fileName);
     }
-    printWriter.close();
-    return writer.toString();
-  }
 
-  public void setCrashCallBack(CrashCallBack crashCallBack) { this.crashCallBack = crashCallBack; }
+    /**
+     * 当UncaughtException发生时会转入该重写的方法来处理
+     */
+    public void uncaughtException(@NonNull Thread thread, @NonNull Throwable throwable) {
 
-  public static interface CrashCallBack {
-    boolean onCrash(Throwable param1Throwable);
-  }
+      if (throwable == null)
+        return ;
+
+        handleException(throwable);
+    }
+
+    /**
+     * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成.
+     *
+     * @param ex 异常信息
+     *           如果处理了该异常信息;否则返回false.
+     */
+    public boolean handleException(Throwable ex) {
+
+        new Thread() {
+            public void run() {
+                Looper.prepare();
+                Toast.makeText(context, "很抱歉,程序出现异常", LENGTH_SHORT).show();
+                Looper.loop();
+            }
+        }.start();
+        // 收集设备参数信息
+        Map<String, String> info = collectDeviceInfo(context);
+
+        // 保存日志文件
+        boolean isSave = true;
+        if (this.crashCallBack != null) {
+            isSave = this.crashCallBack.onCrash(ex);
+        }
+        if (isSave) {
+            saveCrashInfo2File(ex, info);
+        }
+
+        return true;
+    }
+
+    /**
+     * 收集设备参数信息
+     *
+     * @param context
+     */
+    public Map<String, String> collectDeviceInfo(Context context) {
+        Map<String, String> info = new HashMap<String, String>();// 用来存储设备信息和异常信息
+
+        try {
+            PackageManager pm = context.getPackageManager();// 获得包管理器
+            PackageInfo pi = pm.getPackageInfo(context.getPackageName(),
+                    PackageManager.GET_ACTIVITIES);// 得到该应用的信息，即主Activity
+            if (pi != null) {
+                String versionName = pi.versionName == null ? "null"
+                        : pi.versionName;
+                String versionCode = pi.versionCode + "";
+                info.put("versionName", versionName);
+                info.put("versionCode", versionCode);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        Field[] fields = Build.class.getDeclaredFields();// 反射机制
+        for (Field field : fields) {
+            try {
+                field.setAccessible(true);
+                info.put(field.getName(), field.get("").toString());
+                Log.d(TAG, field.getName() + ":" + field.get(""));
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return info;
+    }
+
+
+    /**
+     * 保存异常信息
+     *
+     * @param ex
+     * @return
+     */
+    private String saveCrashInfo2File(Throwable ex, Map<String, String> info) {
+        StringBuffer sb = new StringBuffer();
+        for (Map.Entry<String, String> entry : info.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            sb.append(key + "=" + value + "\r\n");
+        }
+        Writer writer = new StringWriter();
+        PrintWriter pw = new PrintWriter(writer);
+        ex.printStackTrace(pw);
+        Throwable cause = ex.getCause();
+        // 循环着把所有的异常信息写入writer中
+        while (cause != null) {
+            cause.printStackTrace(pw);
+            cause = cause.getCause();
+        }
+        pw.close();// 记得关闭
+        String result = writer.toString();
+        sb.append(result);
+        // 保存文件
+        String time = QLog.getTimeStr("yyyyMMddHHmmss");
+        QLog.e(new Object[]{"CrashHandler", "产生致命异常,见日志:", time, ex.getMessage()});
+        ex.printStackTrace();
+        QLog.addFile(fileCrash,
+                String.format("\r\n============= %s ============\r\n " +
+                                "%s =============================\r\n",
+                        new Object[]{
+                                time, sb.toString()}));
+        return null;
+    }
+
+    private String toString(Throwable ex) {
+        Writer writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        ex.printStackTrace(printWriter);
+        Throwable exCause = ex.getCause();
+        while (exCause != null) {
+            exCause.printStackTrace(printWriter);
+            exCause = exCause.getCause();
+        }
+        printWriter.close();
+        return writer.toString();
+    }
+
+    public void setCrashCallBack(CrashCallBack crashCallBack) {
+        this.crashCallBack = crashCallBack;
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+
+    public static interface CrashCallBack {
+        boolean onCrash(Throwable param1Throwable);
+    }
+
 }
--- QuickLog/src/main/java/com/blxt/quicklog/QLog.java
+++ QuickLog/src/main/java/com/blxt/quicklog/QLog.java
@@ -2,10 +2,12 @@ package com.blxt.quicklog;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.Parcelable;
 import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.blxt.qfile.QZip;
@@ -20,278 +22,324 @@ import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class QLog
-{
-  public static long MAX_LNEG = 10485760L;
+public class QLog {
+    public static long MAX_LNEG = 10485760L;
+    static boolean I = true;
+    static boolean D = true;
+    static boolean W = true;
+    static boolean E = true;
 
-  public static class PATH
-  {
-    public static String SDPath = Environment.getExternalStorageDirectory().getPath();
 
-    public static String getAppFilesPath(Context context) { return context.getFilesDir().getPath(); }
+    public static class PATH {
+        public static String SDPath = Environment.getExternalStorageDirectory().getPath();
 
-    public static String getAppCachePath(Context context) { return context.getCacheDir().getPath(); }
-
-    public static String getAppCacheLogPath(Context context) { return getAppCachePath(context) + "/log.txt"; }
-  }
-
-  static String logFilePath = "";
-  static File file = null;
-
-  public static boolean isSave = true;
-  public static String CODE = "UTF-8";
-  static final String DEFAULT_TAG = "Q-LOG";
-
-  public QLog(@NonNull String defaultpath) {
-    logFilePath = defaultpath;
-    file = new File(logFilePath);
-    File filePath = new File(file.getParent());
-    if (!filePath.exists()) {
-      filePath.mkdirs();
-    }
-  }
-
-  public QLog(Context context) { this(PATH.getAppCacheLogPath(context)); }
-
-  public QLog setMaxSize(long MAX_LNEG) {
-    QLog.MAX_LNEG = MAX_LNEG;
-    return this;
-  }
-
-  public static void i(Object... msg) {
-    Mlg mlg = new Mlg(msg);
-
-    saveLog(String.format("[info]  %s: %s", new Object[] { mlg.tag, mlg.str }));
-    Log.i(mlg.tag, mlg.str);
-  }
-
-  public static void d(Object... msg) {
-    Mlg mlg = new Mlg(msg);
-
-    saveLog(String.format("[debug] %s: %s", new Object[] { mlg.tag, mlg.str }));
-    Log.d(mlg.tag, mlg.str);
-  }
-
-  public static void e(Object... msg) {
-    Mlg mlg = new Mlg(msg);
-
-    saveLog(String.format("[error] %s: %s", new Object[] { mlg.tag, mlg.str }));
-    Log.e(mlg.tag, mlg.str);
-  }
-
-  public static void w(Object... msg) {
-    Mlg mlg = new Mlg(msg);
-
-    saveLog(String.format("[worry] %s: %s", new Object[] { mlg.tag, mlg.str }));
-    Log.w(mlg.tag, mlg.str);
-  }
-
-  public static void f(String format, Object... msg) {
-    Mlg mlg = new Mlg(format, msg);
-    saveLog(String.format("[info] %s: %s", new Object[] { mlg.tag, mlg.str }));
-    Log.i(mlg.tag, mlg.str);
-  }
-
-  static class Mlg
-  {
-    String tag = "Q-LOG";
-    String str = "";
-
-    Mlg(String format, Object... msg) {
-      if (msg.length == 1) {
-        this.str = String.format(format, msg);
-      } else {
-
-        this.tag = getObjStr(msg[0]);
-        Object[] msgN = new Object[msg.length - 1];
-        for (int i = 1; i < msgN.length; i++) {
-          msgN[i - 1] = msg[i];
+        public static String getAppFilesPath(Context context) {
+            return context.getFilesDir().getPath();
         }
 
-        this.str = String.format(format, msgN);
-      }
-    }
-
-    Mlg(Object... msg) {
-      if (msg.length == 1) {
-        this.str = getObjStr(msg[0]);
-      } else {
-
-        this.tag = getObjStr(msg[0]);
-        for (int i = 1; i < msg.length - 1; i++) {
-          this.str += getObjStr(msg[i]) + ",";
+        public static String getAppCachePath(Context context) {
+            return context.getCacheDir().getPath();
         }
-        this.str += getObjStr(msg[msg.length - 1]);
-      }
-    }
 
-    private String getObjStr(Object obj) {
-      if (obj == null) {
-        return "null";
-      }
-
-      if (obj instanceof Exception) {
-        return QLog.getException((Exception)obj);
-      }
-      if (obj instanceof Byte) {
-        return String.format("%X", new Object[] { obj });
-      }
-      if (obj instanceof Short) {
-        return String.format("%X", new Object[] { obj });
-      }
-      if (obj instanceof Integer) {
-        return String.format("%d", new Object[] { obj });
-      }
-      if (obj instanceof Long) {
-        return String.format("%d", new Object[] { obj });
-      }
-      if (obj instanceof Float) {
-        return String.format("%f", new Object[] { obj });
-      }
-      if (obj instanceof Double) {
-        return String.format("%f", new Object[] { obj });
-      }
-      if (obj instanceof Character) {
-        return String.format("%c", new Object[] { obj });
-      }
-      if (obj instanceof String) {
-        return (String)obj;
-      }
-      if (obj instanceof Class) {
-        return ((Class)obj).getClass().getName();
-      }
-      if (obj instanceof Boolean) {
-        return "" + obj.toString();
-      }
-      if (obj instanceof String[]) {
-        String _S = "";
-        for (String s : (String[])obj) {
-          _S = _S + s + ",";
+        public static String getAppCacheLogPath(Context context) {
+            return getAppCachePath(context) + "/log.txt";
         }
-        return _S;
-      }
-
-      return obj.toString();
     }
-  }
 
-  public static void saveLog(String msg) {
-    if (isSave) {
-      addFile(file,
-              String.format("%s %s\r\n", new Object[] {
-                      getTimeStr("<MM-dd HH:mm:ss.S> "), msg
-              }));
+    static String logFilePath = "";
+    static File file = null;
+
+    public static boolean isSave = true;
+    public static String CODE = "UTF-8";
+    static final String DEFAULT_TAG = "Q-LOG";
+
+    public QLog(@NonNull String defaultpath) {
+        logFilePath = defaultpath;
+        file = new File(logFilePath);
+        File filePath = new File(file.getParent());
+        if (!filePath.exists()) {
+            filePath.mkdirs();
+        }
+
     }
-  }
 
-  public static String getTimeStr(String format) {
-    Date currentTime = new Date();
-    SimpleDateFormat formatter = new SimpleDateFormat(format);
-    String dateString = formatter.format(currentTime);
-    return dateString;
-  }
+    public QLog(Context context) {
+        this(PATH.getAppCacheLogPath(context));
 
-  public static synchronized boolean addFile(@NonNull File filepath, @NonNull String content) {
-    FileOutputStream fos = null;
-    OutputStreamWriter osw = null;
-    try {
-      if (!filepath.exists()) {
-        filepath.createNewFile();
-        fos = new FileOutputStream(filepath);
-      } else {
+        if(isDebugVersion(context)){
+            D = false;
+        }
+    }
 
-        fos = new FileOutputStream(filepath, true);
-      }
+    public QLog setMaxSize(long MAX_LNEG) {
+        QLog.MAX_LNEG = MAX_LNEG;
+        return this;
+    }
 
-      osw = new OutputStreamWriter(fos, CODE);
+    public static void i(Object... msg) {
+        if(!I){
+            return;
+        }
+        Mlg mlg = new Mlg(msg);
 
-      osw.write(content);
+        saveLog(String.format("[info ]  %s: %s", new Object[]{mlg.tag, mlg.str}));
+        Log.i(mlg.tag, mlg.str);
+    }
 
-      osw.close();
-    } catch (Exception e) {
-      return false;
-    } finally {
-      if (fos != null) {
+    public static void d(Object... msg) {
+        if(!D){
+            return;
+        }
+        Mlg mlg = new Mlg(msg);
+
+        saveLog(String.format("[debug] %s: %s", new Object[]{mlg.tag, mlg.str}));
+        Log.d(mlg.tag, mlg.str);
+    }
+
+    public static void e(Object... msg) {
+        if(!E){
+            return;
+        }
+        Mlg mlg = new Mlg(msg);
+
+        saveLog(String.format("[error] %s: %s", new Object[]{mlg.tag, mlg.str}));
+        Log.e(mlg.tag, mlg.str);
+    }
+
+    public static void w(Object... msg) {
+        if(!W){
+            return;
+        }
+        Mlg mlg = new Mlg(msg);
+
+        saveLog(String.format("[worry] %s: %s", new Object[]{mlg.tag, mlg.str}));
+        Log.w(mlg.tag, mlg.str);
+    }
+
+    static class Mlg {
+        String tag = "Q-LOG";
+        String str = "";
+
+        Mlg(String format, Object... msg) {
+            if (msg.length == 1) {
+                this.str = String.format(format, msg);
+            } else {
+
+                this.tag = getObjStr(msg[0]);
+                Object[] msgN = new Object[msg.length - 1];
+                for (int i = 1; i < msgN.length; i++) {
+                    msgN[i - 1] = msg[i];
+                }
+
+                this.str = String.format(format, msgN);
+            }
+        }
+
+        Mlg(Object... msg) {
+            if (msg.length == 1) {
+                this.str = getObjStr(msg[0]);
+            } else {
+
+                this.tag = getObjStr(msg[0]);
+                for (int i = 1; i < msg.length - 1; i++) {
+                    this.str += getObjStr(msg[i]) + ",";
+                }
+                this.str += getObjStr(msg[msg.length - 1]);
+            }
+        }
+
+        private String getObjStr(Object obj) {
+            if (obj == null) {
+                return "null";
+            }
+
+            if (obj instanceof Exception) {
+                return QLog.getException((Exception) obj);
+            }
+            if (obj instanceof Byte) {
+                return String.format("%X", new Object[]{obj});
+            }
+            if (obj instanceof Short) {
+                return String.format("%X", new Object[]{obj});
+            }
+            if (obj instanceof Integer) {
+                return String.format("%d", new Object[]{obj});
+            }
+            if (obj instanceof Long) {
+                return String.format("%d", new Object[]{obj});
+            }
+            if (obj instanceof Float) {
+                return String.format("%f", new Object[]{obj});
+            }
+            if (obj instanceof Double) {
+                return String.format("%f", new Object[]{obj});
+            }
+            if (obj instanceof Character) {
+                return String.format("%c", new Object[]{obj});
+            }
+            if (obj instanceof String) {
+                return (String) obj;
+            }
+            if (obj instanceof Class) {
+                return ((Class) obj).getClass().getName();
+            }
+            if (obj instanceof Boolean) {
+                return "" + obj.toString();
+            }
+            if (obj instanceof String[]) {
+                String _S = "";
+                for (String s : (String[]) obj) {
+                    _S = _S + s + ",";
+                }
+                return _S;
+            }
+
+            return obj.toString();
+        }
+    }
+
+    public static void saveLog(String msg) {
+        if (isSave) {
+            addFile(file,
+                    String.format("%s %s\r\n", new Object[]{
+                            getTimeStr("<MM-dd HH:mm:ss.S> "), msg
+                    }));
+        }
+    }
+
+    public static String getTimeStr(String format) {
+        Date currentTime = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat(format);
+        String dateString = formatter.format(currentTime);
+        return dateString;
+    }
+
+    public static synchronized boolean addFile(@NonNull File filepath, @NonNull String content) {
+        FileOutputStream fos = null;
+        OutputStreamWriter osw = null;
         try {
-          fos.close();
-          osw.close();
-        } catch (IOException e) {
-          e.printStackTrace();
+            if (!filepath.exists()) {
+                filepath.createNewFile();
+                fos = new FileOutputStream(filepath);
+            } else {
+
+                fos = new FileOutputStream(filepath, true);
+            }
+
+            osw = new OutputStreamWriter(fos, CODE);
+
+            osw.write(content);
+
+            osw.close();
+        } catch (Exception e) {
+            return false;
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                    osw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-      }
+
+        long size = filepath.length();
+        if (size > MAX_LNEG) {
+
+            File zip = new File(filepath.getParent() + "/" + getNameWithoutFormat(filepath.getPath()) + ".zip");
+
+            if (zip.exists()) {
+                zip.delete();
+            }
+            try {
+                QZip.zipFile(file, zip);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            file.delete();
+        }
+
+        return true;
     }
 
-    long size = filepath.length();
-    if (size > MAX_LNEG) {
-
-      File zip = new File(filepath.getParent() + "/" + getNameWithoutFormat(filepath.getPath()) + ".zip");
-
-      if (zip.exists()) {
-        zip.delete();
-      }
-      try {
-        QZip.zipFile(file, zip);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-      file.delete();
+    public static String getException(Exception e) {
+        Writer writer = null;
+        PrintWriter printWriter = null;
+        try {
+            writer = new StringWriter();
+            printWriter = new PrintWriter(writer);
+            e.printStackTrace(printWriter);
+            return writer.toString();
+        } finally {
+            try {
+                if (writer != null)
+                    writer.close();
+                if (printWriter != null)
+                    printWriter.close();
+            } catch (IOException iOException) {
+            }
+        }
     }
 
-    return true;
-  }
-
-  public static String getException(Exception e) {
-    Writer writer = null;
-    PrintWriter printWriter = null;
-    try {
-      writer = new StringWriter();
-      printWriter = new PrintWriter(writer);
-      e.printStackTrace(printWriter);
-      return writer.toString();
-    } finally {
-      try {
-        if (writer != null)
-          writer.close();
-        if (printWriter != null)
-          printWriter.close();
-      } catch (IOException iOException) {}
+    public static String getNameWithoutFormat(String filePath) {
+        if (filePath.isEmpty()) {
+            return "";
+        }
+        int point = filePath.lastIndexOf('.');
+        return filePath.substring(filePath.lastIndexOf(File.separator) + 1, point);
     }
-  }
 
-  public static String getNameWithoutFormat(String filePath) {
-    if (filePath.isEmpty()) {
-      return "";
+    public static long getLogSize() {
+        if (file == null || !file.exists()) {
+            return 0L;
+        }
+        return file.length();
     }
-    int point = filePath.lastIndexOf('.');
-    return filePath.substring(filePath.lastIndexOf(File.separator) + 1, point);
-  }
 
-  public static long getLogSize() {
-    if (file == null || !file.exists()) {
-      return 0L;
+    public static synchronized boolean delLogFile() {
+        if (file == null || !file.exists()) {
+            return false;
+        }
+        try {
+            file.delete();
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
-    return file.length();
-  }
 
-  public static synchronized boolean delLogFile() {
-    if (file == null || !file.exists()) {
-      return false;
+    public static String openLogFolder(Context context) {
+        Intent shareIntent = new Intent("android.intent.action.SEND");
+        shareIntent.putExtra("android.intent.extra.STREAM",
+                (Parcelable) Uri.fromFile(file));
+        shareIntent.setType("text/plain");
+        context.startActivity(Intent.createChooser(shareIntent, "打开为："));
+
+        return file.getPath();
     }
-    try {
-      file.delete();
-    } catch (Exception e) {
-      return false;
+
+    /**
+     * 获取日志文件
+     *
+     * @return
+     */
+    public static File getLogFile() {
+        return file;
     }
-    return true;
-  }
 
-  public static String openLogFolder(Context context) {
-    Intent shareIntent = new Intent("android.intent.action.SEND");
-    shareIntent.putExtra("android.intent.extra.STREAM",
-            (Parcelable)Uri.fromFile(file));
-    shareIntent.setType("text/plain");
-    context.startActivity(Intent.createChooser(shareIntent, "打开为："));
-
-    return file.getPath();
-  }
+    /**
+     * 判断是否为Debug版本
+     * @param context
+     * @return
+     */
+    public static boolean isDebugVersion(Context context) {
+        try {
+            ApplicationInfo info = context.getApplicationInfo();
+            return (info.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
